b10bd725648fb30bcec7606c23f320f9f09e6d98
Elsie <132709295+Elsiepz27@users.noreply.github.com>
2023-05-22 12:47:58 -0700

    Electrical Room Change

---
+++ Meet Me At The Clock/Assets/Scenes/Eletrical Room/KeyPad.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class KeyPad : MonoBehaviour
+{
+    [SerializeField] private Text Ans;
+
+    private string Answer = "1912";
+
+    public void Number(int number)
+    {
+        Ans.text += number.ToString();
+    }
+
+
+    public void Execute()
+    {
+        if (Ans.text == Answer)
+        {
+            Ans.text = "VALID";
+            SceneManager.LoadScene("Electrical Panel");
+        }
+        else
+        {
+            Ans.text = "ERROR";
+        }
+    }
+
+    public void Delete()
+    {
+        Ans.text = null;
+    }
+
+}   
---
+++ Meet Me At The Clock/Assets/Scenes/Eletrical Room/WireCount.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WireCount : MonoBehaviour
+{
+    static public WireCount Instance;
+
+    public int switchCount;
+    public GameObject winText;
+    private int onCount = 0;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void SwitchChange(int points) {
+        onCount = onCount + points;
+        if (onCount == switchCount)
+        {
+            winText.SetActive(true);
+            // Change Scene
+            SceneManager.LoadScene("Side View 1");
+        }
+
+    }
+}

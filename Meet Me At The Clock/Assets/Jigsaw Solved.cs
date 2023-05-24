60b33e1d22df59e63a3457f05b5ef189f7a17d5a
JanaC08 <134019704+JanaVCh@users.noreply.github.com>
2023-05-24 15:47:09 -0700

    add newspaper boxes

---
+++ Meet Me At The Clock/Assets/Jigsaw Solved.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JigsawSolved : MonoBehaviour
+{
+    private int pointsToWin;
+    public GameObject Pieces;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
